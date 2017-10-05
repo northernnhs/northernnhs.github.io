93bac1897b22759628022dad76074f654ebccbcc
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2017-10-04 22:55:39 -0400

    Updated JS.

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ $(document).ready(function () {
         //console.log(data.ip.toString());
         //alert(data.ip);
         $('#v4').html(data.ip.toString());
-        $('#v6').html('<a href="https://docs.google.com/forms/d/e/1FAIpQLSfDu6nPVmRxvk1AC0jgd9BpVLBKgUoLSAO14wwe6hgSOkJuvQ/viewform?usp=pp_url&entry.1359815136=1FAIpQLSfDu6nPVmRxvk1AC0jgd9BpVLBKgUoLSAO14wwe6hgSOkJuvQ;'+data.ip.toString()+';'+(new Date()).toString()+'">Click here.</a>');
+        $('#v6').html('<a href="https://docs.google.com/forms/d/e/1FAIpQLSfDu6nPVmRxvk1AC0jgd9BpVLBKgUoLSAO14wwe6hgSOkJuvQ/viewform?usp=pp_url&entry.1359815136=1FAIpQLSfDu6nPVmRxvk1AC0jgd9BpVLBKgUoLSAO14wwe6hgSOkJuvQ;'+data.ip.toString()+';'+(new Date()).toString()+';1FAIpQLSfDu6nPVmRxvk1AC0jgd9BpVLBKgUoLSAO14wwe6hgSOkJuvQ">Click here.</a>');
     });
 });
