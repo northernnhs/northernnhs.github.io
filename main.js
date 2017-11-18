79b479e1cf39174a2f004e138ee134279eb7a8ea
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2017-11-17 23:05:14 -0500

    Updated GForm.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 		//JSON.minify(data).toString()
 		//console.log(JSON.stringify(data, null, 2));
 		$('#v4').html(data.ip.toString());
-		$('#v6').html('<a href="https://docs.google.com/forms/d/e/1FAIpQLSfDu6nPVmRxvk1AC0jgd9BpVLBKgUoLSAO14wwe6hgSOkJuvQ/viewform?usp=pp_url&entry.1359815136=1FAIpQLSfDu6nPVmRxvk1AC0jgd9BpVLBKgUoLSAO14wwe6hgSOkJuvQ;'+data.ip.toString()+" from "+data.city.toString()+', '+data.region_name.toString()+' '+data.zip_code.toString()+', '+data.country_name.toString()+'('+data.latitude.toString()+', '+data.longitude.toString()+');'+(new Date()).toString()+';1FAIpQLSfDu6nPVmRxvk1AC0jgd9BpVLBKgUoLSAO14wwe6hgSOkJuvQ">Click here.</a>');
+		$('#v6').html('<a href="https://docs.google.com/forms/d/e/1FAIpQLSeLmDVmEDuoPtnAzhBbQah1iv3n6JuFBDdO3IZDyNzgq5UyNQ/viewform?usp=pp_url&entry.1359815136=1FAIpQLSeLmDVmEDuoPtnAzhBbQah1iv3n6JuFBDdO3IZDyNzgq5UyNQ;'+data.ip.toString()+" from "+data.city.toString()+', '+data.region_name.toString()+' '+data.zip_code.toString()+', '+data.country_name.toString()+'('+data.latitude.toString()+', '+data.longitude.toString()+');'+(new Date()).toString()+';1FAIpQLSeLmDVmEDuoPtnAzhBbQah1iv3n6JuFBDdO3IZDyNzgq5UyNQ">Click here.</a>');
 	});
 	/*$.getJSON("https://jsonip.com/?callback=?", function (data) {
         //console.log(data);
