800c2be398afec3d6a2c10693d2e003d8c10c814
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2018-01-11 06:32:45 -0500

    Updated Files.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 		//JSON.minify(data).toString()
 		//console.log(JSON.stringify(data, null, 2));
 		$('#v4').html(data.ip.toString());
-		$('#v6').html('<a href="https://docs.google.com/forms/d/e/1FAIpQLSeLmDVmEDuoPtnAzhBbQah1iv3n6JuFBDdO3IZDyNzgq5UyNQ/viewform?usp=pp_url&entry.1359815136=1FAIpQLSeLmDVmEDuoPtnAzhBbQah1iv3n6JuFBDdO3IZDyNzgq5UyNQ;'+data.ip.toString()+" from "+data.city.toString()+', '+data.region_name.toString()+' '+data.zip_code.toString()+', '+data.country_name.toString()+'('+data.latitude.toString()+', '+data.longitude.toString()+');'+(new Date()).toString()+';1FAIpQLSeLmDVmEDuoPtnAzhBbQah1iv3n6JuFBDdO3IZDyNzgq5UyNQ">Click here.</a>');
+		$('#v6').html('<a href="https://docs.google.com/forms/d/e/1FAIpQLSfNBkGcNxil1IjLk0DumG1JC0zGsLOpQPY-7IotqJS3NmDBkQ/viewform?usp=sf_link&entry.1359815136=1FAIpQLSfNBkGcNxil1IjLk0DumG1JC0zGsLOpQPY-7IotqJS3NmDBkQ;'+data.ip.toString()+" from "+data.city.toString()+', '+data.region_name.toString()+' '+data.zip_code.toString()+', '+data.country_name.toString()+'('+data.latitude.toString()+', '+data.longitude.toString()+');'+(new Date()).toString()+';1FAIpQLSfNBkGcNxil1IjLk0DumG1JC0zGsLOpQPY-7IotqJS3NmDBkQ">Click here.</a>');
 	});
 	/*$.getJSON("https://jsonip.com/?callback=?", function (data) {
         //console.log(data);
