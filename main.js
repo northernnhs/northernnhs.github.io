5f8ab7df433ed635de0229f5eabc4509e064cbb1
Siddhant Nandkishor Pagariya <siddhant.pagariya@gmail.com>
2018-04-26 07:34:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 		//JSON.minify(data).toString()
 		//console.log(JSON.stringify(data, null, 2));
 		$('#v4').html(data.ip.toString());
-		$('#v6').html('<a href="https://goo.gl/forms/HgzDTQr1DzFUyzfs1/viewform?usp=sf_link&entry.1359815136=1FAIpQLSeIO2OT3ZjaK2gjYg9gOJ7bkhErGpaqjP8nw8bYbEj-4WXOWg;'+data.ip.toString()+' from '+data.city.toString()+', '+data.region_name.toString()+' '+data.zip_code.toString()+', '+data.country_name.toString()+'('+data.latitude.toString()+', '+data.longitude.toString()+');'+(new Date()).toString()+';1FAIpQLSeIO2OT3ZjaK2gjYg9gOJ7bkhErGpaqjP8nw8bYbEj-4WXOWg">Click here.</a>');
+		$('#v6').html('<a href="https://docs.google.com/forms/d/e/1FAIpQLSd6yI7cLRynzr-_22p4yAUCkaMVwV2-jKkW5VNAnbw6tj_T6A/viewform?usp=sf_link&entry.1359815136=1FAIpQLSd6yI7cLRynzr-_22p4yAUCkaMVwV2-jKkW5VNAnbw6tj_T6A;'+data.ip.toString()+' from '+data.city.toString()+', '+data.region_name.toString()+' '+data.zip_code.toString()+', '+data.country_name.toString()+'('+data.latitude.toString()+', '+data.longitude.toString()+');'+(new Date()).toString()+';1FAIpQLSd6yI7cLRynzr-_22p4yAUCkaMVwV2-jKkW5VNAnbw6tj_T6A">Click here.</a>');
 	});
 	/*$.getJSON("https://jsonip.com/?callback=?", function (data) {
         //console.log(data);
